30cf88dc8930ac681814697a77a2d90d18ca438b
atsu-tsu <fdferreira93@gmail.com>
2023-10-25 18:00:16 +0100

    1.Time

--- Assets/Script/SecondsUpdate.cs
+++ Assets/Script/SecondsUpdate.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class SecondsUpdate : MonoBehaviour
 {
+    float timeStartOffSet = 0;
+    bool gotStartTime = false;
+
     void Update()
     {
-
+        if (!gotStartTime)
+        {
+            timeStartOffSet = Time.realtimeSinceStartup;
+            gotStartTime= true;
+        }
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, Time.realtimeSinceStartup - timeStartOffSet);
     }
 }
