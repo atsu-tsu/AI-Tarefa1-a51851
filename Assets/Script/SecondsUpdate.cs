ff2a6564cf2823cfb7745ce1e807cbfcf5508cf6
atsu-tsu <fdferreira93@gmail.com>
2023-10-25 18:07:36 +0100

    2.Normalising Movement with Time

--- Assets/Script/SecondsUpdate.cs
+++ Assets/Script/SecondsUpdate.cs
@@ -6,6 +6,7 @@ public class SecondsUpdate : MonoBehaviour
 {
     float timeStartOffSet = 0;
     bool gotStartTime = false;
+    public float speed = 0.5f;
 
     void Update()
     {
@@ -14,6 +15,6 @@ public class SecondsUpdate : MonoBehaviour
             timeStartOffSet = Time.realtimeSinceStartup;
             gotStartTime= true;
         }
-        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, Time.realtimeSinceStartup - timeStartOffSet);
+        this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, (Time.realtimeSinceStartup - timeStartOffSet) * speed);
     }
 }
