69a1a0eebaa1595e42272bd3f76cfc5dc44622af
atsu-tsu <fdferreira93@gmail.com>
2023-10-25 18:19:51 +0100

    1.Velocity

---
+++ Assets/MoveShell.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveShell : MonoBehaviour
+{
+    
+    float speed = 1f;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        this.transform.Translate(0, Time.deltaTime * speed * 0.5f, Time.deltaTime*speed);
+    }
+}
