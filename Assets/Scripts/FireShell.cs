fbbd45e875f72c3983e6e0962659038e4a839a73
atsu-tsu <fdferreira93@gmail.com>
2023-10-27 01:19:38 +0100

    1.Calculating Trajectories Part 1

---
+++ Assets/Scripts/FireShell.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireShell : MonoBehaviour {
+
+    public GameObject bullet;
+    public GameObject turret;
+    public GameObject enemy;
+
+    private float rotSpeed = 2;
+    
+
+    
+
+    void CreateBullet() 
+    {
+        Instantiate(bullet, turret.transform.position, turret.transform.rotation);
+    }
+
+    void Update() 
+    {
+        Vector3 direction = (enemy.transform.position - this.transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookRotation, Time.deltaTime * rotSpeed);
+        
+
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            CreateBullet();
+            
+        }
+    }
+
+   
+}
